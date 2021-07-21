a2e9ae9b2775958dab76765e4c313cb18227702c
liyuzhao <8603583yuPENG>
2021-07-21 22:43:23 +0800

    update

--- seata-test-api/src/main/java/com/phper666/seata/test/account/provider/rpc/OrderDubboService.java
+++ seata-test-api/src/main/java/com/phper666/seata/test/account/provider/rpc/OrderDubboService.java
@@ -4,14 +4,6 @@ package com.phper666.seata.test.account.provider.rpc;
 import com.phper666.seata.test.account.provider.dto.OrderDTO;
 import com.phper666.seata.test.account.provider.response.ObjectResponse;
 
-/**
- * <p>
- *  创建订单
- * </p>
- *
- * * @author lidong
- * @since 2019-09-04
- */
 public interface OrderDubboService {
 
     /**
